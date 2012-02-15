007a7b02d0d3085f0a0b39c841a4711f9b7c36ba
Sergii Grebeniuk <grebenjuk.s@gmail.com>
2012-02-15 13:25:00 +0200

    reverted test changes

--- applications/main/controllers/about.php
+++ applications/main/controllers/about.php
@@ -3,12 +3,11 @@
 namespace Applications\Main\Controllers;
 
 use Engine;
-use Applications\Main\Models;
 
 class About extends Engine\Controller
 {
     public function index()
     {
-        $this->template->content = 'about '.Models\User::find_by_id(1)->name;
+        $this->template->content = 'about';
     }
 }
