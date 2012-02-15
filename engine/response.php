bb29a248c21de0f1ce2bae060c3c8358af4ef89e
Sergii Grebeniuk <grebenjuk.s@gmail.com>
2012-02-15 13:30:49 +0200

    cleaning up Response class

--- engine/response.php
+++ engine/response.php
@@ -51,10 +51,6 @@ class Response
         $route = new Route($this->request, $this->application_name);
 
         $controller_name  = 'Applications\\'.ucfirst($this->application_name).'\\Controllers\\'.ucfirst($route->controller);
-        if ( ! class_exists($controller_name))
-        {
-            $controller_name  = str_replace('Controllers\\', 'Controllers_', $controller_name);
-        }
         $controller       = new $controller_name($this->application_name);
         $action           = $route->action;
         $parameters       = $route->parameters;
