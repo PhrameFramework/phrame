5538bdfb20aa5b6882ff4f0ef9f9a8d90ac5df75
Sergii Grebeniuk <grebenjuk.s@gmail.com>
2012-02-28 15:48:42 +0200

    codeception package as submodule

--- phrame/codeception/vendor/bin/build.php
+++
@@ -1,6 +0,0 @@
-<?php
-chdir(__DIR__.'/..');
-system('php bin/update_docs.php');
-system('php bin/build_site.php');
-system('php bin/build_pear.php');
-system('php bin/build_phar.php');
--- phrame/codeception/vendor/tests/acceptance/Cli/GenerateSuiteCept.php
+++
@@ -1,9 +0,0 @@
-<?php
-$I = new CliGuy($scenario);
-$I->wantTo('generate sample Suite');
-$I->amInPath('tests/data/sandbox');
-$I->executeCommand('generate:suite house HouseGuy');
-$I->seeFileFound('house.suite.yml');
-$I->seeInThisFile('class_name: HouseGuy');
-$I->seeFileFound('HouseHelper.php');
-$I->seeFileFound('_bootstrap.php','tests/house');
