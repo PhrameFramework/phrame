35d31872244401fa5dfb34ef495c622a0a2360cf
delmot <grebenjuk.s@gmail.com>
2012-03-06 02:05:07 +0200

    updating main home view

--- applications/main/themes/default/home.php
+++ applications/main/themes/default/home.php
@@ -1,20 +1,29 @@
 <div class="page-header">
     <h1><?php echo $name; ?></h1>
-    <h2>PHP 5.3 Framework</h2>
+    <p>PHP 5.3 Framework</p>
 </div>
 
-<p>Clone repository:</p>
+<h2>Clone repository:</h2>
+
 <pre>
 <strong>$</strong> git clone --recursive https://github.com/PhrameFramework/phrame
 </pre>
 
-<p>Make <b>assets</b> directory writable:</p>
+<hr />
+
+<h2>Installation:</h2>
+
+<p><a href="https://github.com/PhrameFramework/phrame/zipball/master">Download</a> and extract the latest version</p>
+
+<p>Make <strong>assets</strong> directory writable:</p>
+
 <pre>
 <strong>$</strong> cd phrame
 <strong>$</strong> chmod -R 777 public/assets
 </pre>
 
-<p>Install packages from <a href="http://packagist.org/packages/phrame/">Packagist</a>:</p>
+<p>Install packages from <a href="http://packagist.org/packages/phrame/">Composer</a>:</p>
+
 <pre>
 <strong>$</strong> curl -s http://getcomposer.org/installer | php
 <strong>$</strong> php composer.phar install
