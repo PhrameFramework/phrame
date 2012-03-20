f4562ebc18e8d5db9a91fa1b39875e3aaf8492d5
delmot <grebenjuk.s@gmail.com>
2012-03-20 22:30:16 +0200

    added body property to Response class

--- applications/main/controllers/blog.php
+++ applications/main/controllers/blog.php
@@ -9,13 +9,13 @@ class Blog extends Core\Controller
     public function index()
     {
         // This shows how to handle a specific uri in another application and to get some layout's variable
-        $this->layout->content = Core\Application::instance('blog')->response('/')->body()->content;
+        $this->layout->content = Core\Application::instance('blog')->response('/')->body->content;
     }
 
     public function post($id)
     {
         // This shows how to handle request in another application and to get some layout's variable
-        $this->layout->content = Core\Application::instance('blog')->response()->body()->content;
+        $this->layout->content = Core\Application::instance('blog')->response->body->content;
     }
 
     public function comment()
--- applications/main/controllers/documentation.php
+++ applications/main/controllers/documentation.php
@@ -9,6 +9,6 @@ class Documentation extends Core\Controller
     public function index()
     {
         // This shows how to handle a specific uri in another application and to get some layout's variable
-        $this->layout->content = Core\Application::instance('documentation')->response('/')->body()->content;
+        $this->layout->content = Core\Application::instance('documentation')->response('/')->body->content;
     }
 }
