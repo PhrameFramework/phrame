bd45f3622f478ad5acfc75bb041d4b8956621105
Sergii Grebeniuk <grebenjuk.s@gmail.com>
2012-03-28 13:24:08 +0300

    introduced Applications class

--- applications/main/controllers/blog.php
+++ applications/main/controllers/blog.php
@@ -9,18 +9,18 @@ class Blog extends Core\Controller
     public function index()
     {
         // This shows how to handle a specific uri in another application and to get some layout's variable
-        $this->layout->content = Core\Application::instance('blog')->response('/')->body->content;
+        $this->layout->content = Core\Applications::instance('blog')->response('/')->body->content;
     }
 
     public function post($id)
     {
         // This shows how to handle request in another application and to get some layout's variable
-        $this->layout->content = Core\Application::instance('blog')->response->body->content;
+        $this->layout->content = Core\Applications::instance('blog')->response->body->content;
     }
 
     public function comment()
     {
         // Simple handling of a specific uri in another application
-        Core\Application::instance('blog')->response('post/comment')->body();
+        Core\Applications::instance('blog')->response('post/comment')->body();
     }
 }
