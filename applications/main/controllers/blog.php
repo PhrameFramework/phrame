3031a82e7d63ed5896beaf346e8fc59b18bb409a
Sergii Grebeniuk <grebenjuk.s@gmail.com>
2012-04-06 13:25:40 +0300

    specific uri handling is back

--- applications/main/controllers/blog.php
+++ applications/main/controllers/blog.php
@@ -9,7 +9,7 @@ class Blog extends Core\Controller
     public function index()
     {
         // This shows how to handle a specific uri in another application and to get some layout's variable
-        $this->layout->content = Core\Applications::instance('blog')->response->body->content;
+        $this->layout->content = Core\Applications::instance('blog')->response('/')->body->content;
     }
 
     public function post($id)
