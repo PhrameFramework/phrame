ddffc8e5787e1500fa403710ff5c74224ae807ae
Sergii Grebeniuk <grebenjuk.s@gmail.com>
2012-04-19 18:20:25 +0300

    using new methods in blog app

--- applications/main/controllers/blog.php
+++ applications/main/controllers/blog.php
@@ -14,13 +14,13 @@ class Blog extends Core\Controller
 
     public function post($id)
     {
-        // This shows how to handle request in another application and to get some layout's variable
-        $this->layout->content = Core\Applications::instance('blog')->response->body->content;
+        // This shows how to handle request in another application and to get content
+        $this->layout->content = Core\Applications::content('blog');
     }
 
     public function comment()
     {
         // Simple handling of a specific uri in another application
-        Core\Applications::instance('blog/post/comment')->run();
+        Core\Applications::run('blog/post/comment');
     }
 }
